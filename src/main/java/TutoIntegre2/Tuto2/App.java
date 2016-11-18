62c1648d5893441a37beec61467e67a17e041a97
luc0152 <luc0152@0.51.81.50>
2016-11-18 15:20:27 +0100

    début

---
+++ src/main/java/TutoIntegre2/Tuto2/App.java
@@ -0,0 +1,13 @@
+package TutoIntegre2.Tuto2;
+
+/**
+ * Hello world!
+ * Fonction main
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/TutoIntegre2/Tuto2/CodeMort.java
@@ -0,0 +1,11 @@
+package TutoIntegre2.Tuto2;
+
+public class CodeMort {
+	
+	public String mort;
+	
+	public void mort(){
+		
+	}
+
+}
